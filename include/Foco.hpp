0c42e7fdc1ecea3b78df5393d10fbae530db6eb7
Belén Abigail Aguilar Flores <a24110177@live.ceti.mx>
2025-09-18 01:55:20 +0000

    Practica1

---
+++ include/Foco.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <EstadoFoco.hpp>
+
+class {
+
+    private:
+        EstadoFoco estadoActual;
+    public:
+        Foco () {}
+        ~ Foco () {}
+        void Encender () {}
+        void Apagar () {}
+    EstadoFoco MostrarEstado () {}
+};
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const*argv[]){
+    cout<<"Hola mundo"<<endl;
+    return 0;
+}
